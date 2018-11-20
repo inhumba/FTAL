dc600c3f0547cd374dbaf14befd280b01f130103
Pathompong Sangkajit <p.inhumba@gmail.com>
2018-11-20 10:28:56 +0700

    Initial setup homepage

---
+++ footer.php
@@ -0,0 +1,19 @@
+</div><!--#content-->
+	
+	<div class="site-footer-space"></div>
+	<footer id="colophon" class="site-footer">
+		<div class="container">
+			<div class="site-info">
+				Copyright &copy; <?php echo date("Y"); ?> FTAL CO., LTD. All rights reserved.
+			</div><!--site-info-->
+		</div><!--container-->
+	</footer><!--site-footer-->
+
+<?php //endif; ?>
+
+</div><!--#page-->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ page-home.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * The template for displaying home page.
+ *
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package plant
+ */
+
+get_header();?>
+
+<div class="container">
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main -hide-title">
+
+			<?php while ( have_posts() ) : the_post(); ?>
+				
+				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+				
+				<?php
+					// If comments are open or we have at least one comment, load up the comment template.
+					if ( comments_open() || get_comments_number() ) :
+						comments_template();
+					endif;
+				?>
+
+			<?php endwhile; // End of the loop. ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+</div><!--container-->
+<?php get_footer(); ?>
